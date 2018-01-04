71f7dd1241e5c2bbb77658f1e050833feb120701
cleverzhang(张超) <cleverzhang@tencent.com>
2018-01-04 16:23:05 +0800

    4.0.1版本提交

--- TencentMapSdk/Framework/TMapBeaconAPI_SDKBase.framework/Headers/BeaconBaseInterface.h
+++ TencentMapSdk/Framework/TMapBeaconAPI_SDKBase.framework/Headers/BeaconBaseInterface.h
@@ -16,16 +16,16 @@
 
 //1 fetal 2 error 3 warn 4 info
 //in debug version: 5 debug 10 all
-+ (void) setLogLevel:(int) logLevel;
++ (void)setLogLevel:(int)logLevel;
 
 //获取当前sdk'的运行状态
-+ (sdkstatus) getSDKStatus;
++ (sdkstatus)getSDKStatus;
 
 //得到灯塔sdk的版本
-+(NSString*) getBeaconSDKVersion;
++ (NSString *)getBeaconSDKVersion;
 
 //启用事件上报功能
-+ (BOOL) enableEventRecord:(BOOL) enable;
++ (BOOL)enableEventRecord:(BOOL)enable;
 
 //@end
 
@@ -36,10 +36,10 @@
 
 //设置sdk进行与服务器同步的延时（初次启动不会有延时，如果上次发生crash也不会延迟）
 //主要是控制sdk在启动过程中占用更多资源
-+ (void) setSynchServerTimerDelay: (int) delay;
++ (void)setSynchServerTimerDelay:(int)delay;
 
 //注册监控服务，将会向服务器查询策略，并初始化各个模块的默认数据上传器，参数userId为用户qua，参数gateWayIP为当前网络网关IP，不填则默认使用服务器下发的IP
-+ (BOOL) enableAnalytics :(NSString *) userId gatewayIP:(NSString *) gatewayIP;
++ (BOOL)enableAnalytics:(NSString *)userId gatewayIP:(NSString *)gatewayIP;
 
 //@end
 
@@ -51,19 +51,19 @@
 //注册监控服务，不会向服务器查询策略，需要客户端主动设置，可以调用setStrategyForMaxPkgSize
 //需要客户端调用getMixDataUploader等接口自己获取数据，并在上传完成后通知sdk
 //客户端可以在任意时刻调用接口获取数据，也可以注册AnaUplaodNotifier协议以便能收到sdk提供的存储数据量达到上限的通知以便能及时上传，因为sdk对存储在本地的数据量时有限制的，超过的将不予存储
-+ (BOOL) enableAnalyticsWithoutNetwork :(NSString *) userId gatewayIP:(NSString *) gatewayIP;
++ (BOOL)enableAnalyticsWithoutNetwork:(NSString *)userId gatewayIP:(NSString *)gatewayIP;
 
 //获取各种类型数据的上传者，从上传者那里可以得到上传的数据，
 //getMixData
-+ (id<TMapBeaconAnaUploader>) getMixDataUploader;
++ (id<TMapBeaconAnaUploader>)getMixDataUploader;
 //getRealTimeEventData
-+ (id<TMapBeaconAnaUploader>) getRealTimeEventDataUploader;
++ (id<TMapBeaconAnaUploader>)getRealTimeEventDataUploader;
 
 
 //注册一个混合数据到达上限的监听器，以便能接收信号
-+ (BOOL) registMixDataUploadNotifier:(id<TMapAnaUplaodNotifier>) notifier;
++ (BOOL)registMixDataUploadNotifier:(id<TMapAnaUplaodNotifier>)notifier;
 //注册一个实时事件到达上限的监听器，以便能接收信号
-+ (BOOL) registRealTimeEventDataUploadNotifier:(id<TMapAnaUplaodNotifier>) notifier;
++ (BOOL)registRealTimeEventDataUploadNotifier:(id<TMapAnaUplaodNotifier>)notifier;
 
 //@end
 
@@ -75,24 +75,24 @@
 
 //用户事件上报通用接口，该接口会保存用户事件到本地，根据上报策略择机上报
 //event 事件名称 isSucceed 事件执行是否成功 elapse 事件执行耗时，单位ms size 上报包大小，单位kb params 其他参数，用户自定义
-+ (BOOL) onUserAction: (NSString*) eventName isSucceed:(BOOL) isSucceed elapse:(long) elapse size:(long) size params:(NSDictionary *) params;
++ (BOOL)onUserAction:(NSString *)eventName isSucceed:(BOOL)isSucceed elapse:(long)elapse size:(long)size params:(NSDictionary *)params;
 
 //实时用户事件上报通用接口，该接口会保存用户事件实时上报
 //event 事件名称 isSucceed 事件执行是否成功 elapse 事件执行耗时，单位ms size 上报包大小，单位kb params 其他参数，用户自定义
-+ (BOOL) onDirectUserAction: (NSString*) eventName isSucceed:(BOOL) isSucceed elapse:(long) elapse size:(long) size params:(NSDictionary *) params;
++ (BOOL)onDirectUserAction: (NSString *)eventName isSucceed:(BOOL)isSucceed elapse:(long)elapse size:(long)size params:(NSDictionary *)params;
 // 1.8.7 立即上报接口
-+ (BOOL) onDirectUserAction: (NSString*) eventName isImmediately:(BOOL)isImmediately isSucceed:(BOOL) isSucceed elapse:(long) elapse size:(long) size params:(NSDictionary *) params;
++ (BOOL)onDirectUserAction: (NSString *)eventName isImmediately:(BOOL)isImmediately isSucceed:(BOOL)isSucceed elapse:(long)elapse size:(long)size params:(NSDictionary *)params;
 
 //设置实时事件上报的间隔,默认为1分钟,单位是分钟  最小1分钟 最大10分钟
-+ (BOOL) setRealTimeEventUploadDuration:(int) minitues;
++ (BOOL)setRealTimeEventUploadDuration:(int)minitues;
 //!!!
 //!!!注意，设置时间太小会带来更多的网络请求，占用更多系统资源，在实时事件使用比较频繁的情况下会带来更多风险
 //!!!
-+ (void) setUserRealEventDurationSecond:(int) seconds ;
++ (void)setUserRealEventDurationSecond:(int)seconds ;
 
 //设置实时事件上报的最大累计上报条数（满足条数即上报)
 //默认分别为10，保护区间<=50 >＝5
-+ (BOOL) setRealTimeEventUploaMaxCount:(int) maxPkgSize;
++ (BOOL)setRealTimeEventUploaMaxCount:(int)maxPkgSize;
 
 //@end
 
@@ -102,86 +102,70 @@
 //@interface AnalyticsInterface(UserPorperty)
 
 //设置appKey（灯塔事件）
-+ (void) setAppKey:(NSString*) appKey;
++ (void)setAppKey:(NSString *)appKey;
 
 //设置一个GUID的标识,用以通过GUID标识和分类异常用户信息
-+ (void) setGUID:(NSString*) guid;
++ (void)setGUID:(NSString *)guid;
 
 //更换用户时设置userId,一般是QQ号
-+ (void) setUserId:(NSString*) userId;
-
-//用户自定义的id
-+ (void) setUserDefinedId:(NSString*) userId;
++ (void)setUserId:(NSString *)userId;
 
 //QIMEI
-+ (void) setQIMEI:(NSString*) qimei;
-+(NSString*) getQIMEI;
++ (void)setQIMEI:(NSString *)qimei;
++ (NSString *)getQIMEI;
 
 //sessionid 设置sessionid可以帮您区分哪些事件属于同一次会话
-+ (void) setSessionid:(NSString*) sessionid;
-+(NSString*) getSessionid;
-//QQ 设置QQ
-+ (void) setQQ:(NSString*) qq;
-+ (NSString*) getQQ;
++ (void)setSessionid:(NSString *)sessionid;
++ (NSString *)getSessionid;
 
 //设置MSF的appId
-+ (void) setAppId:(NSString*) appId;
++ (void)setAppId:(NSString*)appId;
 
 //设置渠道（灯塔事件）
-+(void) setChannelId:(NSString*) chanId;
++ (void)setChannelId:(NSString*)chanId;
 
 //设置SDK的版本号，宿主appVersion设置到A95扩展字段
 + (void)setSDKVersion:(NSString *)bundleVer;
 
 //设置联系方式 ,only for crash report
-+ (void) setContact:(NSString*) contact;
++ (void)setContact:(NSString *)contact;
 
 //设置appVersion
-+ (void) setAppVersion:(NSString*) bundleVer;
++ (void)setAppVersion:(NSString *)bundleVer;
 
-+ (void) setGatewayIp:(NSString*) gateWayIp;
++ (void)setGatewayIp:(NSString *)gateWayIp;
 
 //重新设置db路径并且重组数据库,可以在analytics启动之后动态设置
-+ (BOOL) setDBPathAndReBuild:(NSString*) dbPath error:(NSError*) err;
++ (BOOL)setDBPathAndReBuild:(NSString *)dbPath error:(NSError *)err;
 
 //重新设置db路径,必须在enableAnalytics之前调用
-+ (BOOL) setDBPath:(NSString*) dbPath;
++ (BOOL)setDBPath:(NSString *)dbPath;
 //@end
 
 /**
  能够控制策略的接口
  **/
-//@interface AnalyticsInterface(StrategyControl)
-
 //该开关为YES的时候会在本地策略生效的时候就开启功能开关（测速／异常），等服务器策略生效后再更新一次
 //如果为NO那么会等到服务器策略生效后才开启功能开关
 //影响范围：服务器策略生效之前的事件记录和异常处理函数的问题注册
-+ (BOOL) enableModuleFunctionBeforeSeverStrategy:(BOOL) enable;
++ (BOOL)enableModuleFunctionBeforeSeverStrategy:(BOOL)enable;
 
 //设置事件上报的最大累计上报条数（满足条数即上报)，及本地数据库的最大容量（超过限额不予存储）
 //默认分别为50 200，保护区间20～100, 100～200
 //注意：采用默认调用方式，sdk会在与服务器通讯获取策略后更新本地策略，所以该模式下调用该API无效
-+ (BOOL) setStrategyForMaxPkgSize:(int) maxPkgSize dbMaxSize:(int) dbMaxSize;
++ (BOOL)setStrategyForMaxPkgSize:(int)maxPkgSize dbMaxSize:(int)dbMaxSize;
 
 //设置每天最多执行测速的次数，默认是-1不加限制
-+ (BOOL) setDayMaxSpeedTrackCount:(int) count;
-
-//@end
-
++ (BOOL)setDayMaxSpeedTrackCount:(int)count;
 
 /**
  测试接口，只在Test的模式下生效
  **/
-//@interface AnalyticsInterface(Test)
-
 // 这是控查询serverUlr
-+ (BOOL) setAnalyticsServer:(NSString*) serverUrl;
++ (BOOL)setAnalyticsServer:(NSString *)serverUrl;
 
 //开启和关闭灯塔上报（包括属性采集／启动上报／退出事件／使用状况上报）
-+ (BOOL) setEnableBeaconReport:(BOOL) enabled;
-
-//是否刷新APN
-//+(BOOL) setRefresAPN:(BOOL) enabled;
++ (BOOL)setEnableBeaconReport:(BOOL)enabled;
 
 /**
  通用参数, 设置所有事件都会带的参数
@@ -197,8 +181,8 @@
 /**
  * 只在wifi下上报事件接口
  */
-+ (BOOL) onUserAction: (NSString*) eventName isSucceed:(BOOL) isSucceed elapse:(long) elapse size:(long) size params:(NSDictionary *) params isOnlyWifiUpload:(BOOL)isOnlyWifiUpload;
-+ (BOOL) onDirectUserAction: (NSString*) eventName isSucceed:(BOOL) isSucceed elapse:(long) elapse size:(long) size params:(NSDictionary *) params isOnlyWifiUpload:(BOOL)isOnlyWifiUpload;
++ (BOOL)onUserAction: (NSString*) eventName isSucceed:(BOOL) isSucceed elapse:(long) elapse size:(long) size params:(NSDictionary *) params isOnlyWifiUpload:(BOOL)isOnlyWifiUpload;
++ (BOOL)onDirectUserAction: (NSString*) eventName isSucceed:(BOOL) isSucceed elapse:(long) elapse size:(long) size params:(NSDictionary *) params isOnlyWifiUpload:(BOOL)isOnlyWifiUpload;
 
 /**
  * 临时关掉灯塔上报, 达到上报条件的事件先存数据库
@@ -235,5 +219,15 @@
  * 是否开启ATS，默认是开启
  */
 + (void)setAtsEnable:(BOOL)isAtsEnable;
+
+/**
+ * 路径分析功能开启/关闭接口，默认开启此功能
+ */
++ (void)enblePagePath:(BOOL)yesOrNo;
+
+/**
+ * Socket上报开启/关闭接口，默认开启
+ */
++ (void)setSocketOnOff:(BOOL)yesOrNo;
 
 @end
